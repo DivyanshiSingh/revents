6eef039fe8edcd5ae1c8fe12c2157e0470515059
Divyanshi Singh <divyanshipilot@gmail.com>
2020-08-02 08:04:09 +0530

    event managing site

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './app/layout/App';
 import * as serviceWorker from './serviceWorker';
+const rootEl=document.getElementById('root');
+let render=()=>{
+  ReactDOM.render(<App />, rootEl)
+}
+if (module.hot) {
+  module.hot.accept('./app/layout/App',()=>{
+    setTimeout(render);
+  })
+}
+render();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
